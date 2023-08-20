875a94ead9ff7a8692ab109ce6cd61d52428acd8
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-08-20 10:45:20 -0400

    Toma en cuenta todos los anios divisibles por 400 son anios bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,5 +1,5 @@
 function esBisiesto(anio) {
-  if(anio === 4){
+  if(anio%400 === 0){
     return "Es bisiesto";
   }
   return "No es bisiesto";
--- src/esBisiesto.test.js
+++ src/esBisiesto.test.js
@@ -1,10 +1,11 @@
 import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-  it("El 4 deberia considerarse como bisiesto", () => {
-    expect(esBisiesto(4)).toEqual("Es bisiesto");
-  });
+
   it("El 5 no deberia considerarse como bisiesto", () => {
     expect(esBisiesto(5)).toEqual("No es bisiesto");
+  });
+  it("Anios divisibles por 400 deberia considerarse como bisiesto", () => {
+    expect(esBisiesto(2000)).toEqual("Es bisiesto");
   });
 });

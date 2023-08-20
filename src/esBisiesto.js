404552deff7c5de1bd5497894dc52cdb141b65f6
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-08-20 11:08:40 -0400

    Toma en cuenta todos los anios divisibles por 100 pero no por 400 no son anios bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -2,6 +2,10 @@ function esBisiesto(anio) {
   if(anio%400 === 0){
     return "Es bisiesto";
   }
+  if(anio%100 === 0){
+    return "No es bisiesto";
+  }
+
   return "No es bisiesto";
 }
 
--- src/esBisiesto.test.js
+++ src/esBisiesto.test.js
@@ -1,11 +1,17 @@
 import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-
+  // it("El 4 deberia considerarse como bisiesto", () => {
+  //   expect(esBisiesto(4)).toEqual("Es bisiesto");
+  // });
   it("El 5 no deberia considerarse como bisiesto", () => {
     expect(esBisiesto(5)).toEqual("No es bisiesto");
   });
   it("Anios divisibles por 400 deberia considerarse como bisiesto", () => {
     expect(esBisiesto(2000)).toEqual("Es bisiesto");
   });
+  it("Anios divisibles por 100 pero no por 400 deberia considerarse como no bisiesto", () => {
+    expect(esBisiesto(1700)).toEqual("No es bisiesto");
+  });
+
 });
